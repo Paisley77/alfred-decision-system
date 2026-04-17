d72aa7752f85a55c513c2bd55f73fd2386dbe78f
Paisley <hjxleaf@hotmail.com>
2026-04-16 20:04:35 -0400

    Changed host to 0.0.0.0

--- app.py
+++ app.py
@@ -94,4 +94,7 @@ def examples():
     return jsonify(get_all_examples())
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
